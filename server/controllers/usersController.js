03e59f5d9c984950eb6dcf91beacb9f9a4e52e5e
Ericaphann <ericaphann@gmail.com>
2026-04-05 19:31:32 -0400

    Added Postgres stuff

---
+++ server/controllers/usersController.js
@@ -0,0 +1,26 @@
+const pool = require('../db');
+
+// Get all users
+exports.getUsers = async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM users');
+    res.json(result.rows);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
+
+// Create user
+exports.createUser = async (req, res) => {
+  const { name, email } = req.body;
+
+  try {
+    const result = await pool.query(
+      'INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *',
+      [name, email]
+    );
+    res.json(result.rows[0]);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
