a5230626cd11605b85e07b4d298450cfa66edbb1
monica <monicaphann1@gmail.com>
2026-04-16 23:11:23 -0400

    changes app.js route

--- server/src/app.js
+++ server/src/app.js
@@ -58,7 +58,7 @@ const path = require("path");
 app.use(express.static(path.join(__dirname, "../../client/dist")));
 
 app.get("/{*splat}", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
 });
 
 module.exports = app;
